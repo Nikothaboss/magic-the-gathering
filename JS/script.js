c19d871b46b67903980777d141668971c9f8aaff
Nikothaboss <57292271+Nikothaboss@users.noreply.github.com>
2021-09-29 15:22:57 +0200

    made smaller functions and reorganized the use of the functions

--- JS/script.js
+++ JS/script.js
@@ -1,97 +1,53 @@
 import { items, collection } from "./data.js";
+import { findIndex, createCard } from "./utils.js";
 const container = document.querySelector(".container");
 const collCon = document.querySelector(".collection-container")
 
 
-const setDeleteEvent = () =>{
-    const delBtns = document.querySelectorAll(".delBtn");
-
-    for(const btn of delBtns){
-        btn.addEventListener("click", (e)=>{
-            const btnID = e.target.id
-            const index  = items.findIndex(item => item.name === btnID)
-            items.splice(index, 1)  
-            displayCards(items)
-        })
-    }
-}
 
 
 
-const appendListeners = () =>{
-    const buttons = document.querySelectorAll(".cardBtn")
-    
+// ! Event functions
 
-    for(const button of buttons) {
-        button.addEventListener("click", (e) =>{
-            collection.push(e.target.id)
-            
+const addListener = () =>{
+    for(const item of items){
+        document.getElementById(`${item.id}-add`).addEventListener("click", () =>{
+            collection.push(items[findIndex(items, item)])
+
             renderCollection()
         })
     }
+} 
 
+const removeListeners = () =>{
+    for(const item of collection){
+        document.getElementById(`${item.id}-remove`).addEventListener("click", ()=>{
+            collection.splice(findIndex(collection, item), 1);
+            renderCollection()
+        })
+    }
 }
+
+// ! Render functions
 
 const renderCollection = () =>{
     collCon.innerHTML = "";
     
     for(const coll of collection){
         console.log(coll)
-        collCon.innerHTML += `<p>${coll}</p>`
+        collCon.innerHTML += createCard(coll, "remove")
     }
+    removeListeners()
     
 }
 
-// ! Render funksjon
-
-const displayCards = (array) =>{
-    container.innerHTML = ""
-    let neweDiv = ``
-    for(const item of array){
-        neweDiv += `
-            <div class="card">
-                <img src="${item.img}" alt="something" />
-                <div class="content">
-                    <h2>${item.name}</h2>
-                    <p>Race: ${item.race}</p>
-                    <p>Type: ${item.type}</p>
-                    <button class="delBtn" id="${item.name}">Delete</button>
-                    <button type="button" id="${item.id}" class="cardBtn">Add me</button>
-                </div>
-            </div>
-        `
-    }
-    container.innerHTML = neweDiv
-    setDeleteEvent()
-    appendListeners()
+const renderCards = () =>{
+    container.innerHTML = "";
+    items.forEach((item)=>{
+        container.innerHTML += createCard(item, "add")
+    })
+    addListener()
 }
 
-displayCards(items)
-
-
-
-
-
-
-
-
-
-
-
-
-// const arrayRemove =(arr, value)=> { 
-    
-//     return arr.filter(function(ele){ 
-//         return ele != value; 
-//     });
-// }
-
-
-
-
-
-
-
-
-
+renderCards()
 
---
+++ JS/utils.js
@@ -0,0 +1,21 @@
+export const findIndex = (array, item) => {
+    return array.map((x) =>{
+        return x.id
+    }).indexOf(item.id)
+}
+
+export const createCard = (item, version) =>{
+    const {id, name, race, type, img} = item; 
+    return `
+    <div class="card">
+        <img src="${img}" alt="something" />
+        <div class="content">
+            <h2>${name}</h2>
+            <p>Race: ${race}</p>
+            <p>Type: ${type}</p>
+            <button type="button" id="${id}-${version}" class="cardBtn">${version === "add" ? "Add" : "Remove"}</button>
+        </div>
+    </div>
+
+    `
+}
